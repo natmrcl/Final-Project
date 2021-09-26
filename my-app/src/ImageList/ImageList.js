c01f919ede03e1423df4f9fb6db1c688b771b133
Thor Fatih <73384671+ThorTekling@users.noreply.github.com>
2021-09-26 19:12:55 +0700

    Update ImageList.js

--- my-app/src/ImageList/ImageList.js
+++ my-app/src/ImageList/ImageList.js
@@ -31,30 +31,3 @@ const ImageList = ({images}) => {
 }
 
 export default ImageList
-
-// const ImageList = ({ images }) => {
-//     console.log(images)
-//     return (
-//       <div className="container">
-//         <div className="row">
-//           { images.map(image => {
-//             return (
-//               <div key={image.id} className="col-md-4" style={{ marginBottom:"2rem" }}>
-//                 <div className="imageList__container">
-//                   <img className="imageList__image" src={image.largeImageURL} alt={image.tags} />
-//                 </div>
-//                 <div className="image__details">
-//                 <Link to={{ 
-//                     pathname: `/image/${image.id}`,
-//                     state: { image }
-//                   }}><button>View</button></Link>
-//                 </div>
-//               </div>
-//             )
-//           }) }
-//         </div>
-//       </div>
-//     )
-//   }
-  
-//   export default ImageList
